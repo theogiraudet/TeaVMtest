433060d33cb3e40f7aad9a6a3c1532c3d94555c1
Giraudet Théo <theo.giraudet@etudiant.univ-rennes1.fr>
2021-09-26 18:43:39 +0200

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.teavm.jso.dom.html.HTMLDocument;
+import org.teavm.jso.dom.html.HTMLElement;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
